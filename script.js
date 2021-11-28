6682f36ccc086e664af0568118639405594510f2
Enã Lins <ena.mlins@gmail.com>
2021-11-28 20:53:17 -0300

    DONE: carousel dots for navigation

--- script.js
+++ script.js
@@ -1,32 +1,70 @@
 //FIRST SETUP
-let myCarousel = document.querySelector('#myCarousel')
+const
+  myCarousel = document.querySelector('#myCarousel'),
+  scroller = myCarousel.querySelector('.scroller'),
+  slides = scroller.querySelectorAll('.mySlide');
 
-let prev = document.createElement('a');
-let next = document.createElement('a');
+let 
+  slideIndex = 1,
+  dots = [],
+  prev = document.createElement('a'),
+  next = document.createElement('a');
+
+
+// ARROWS CONFIG
 prev.innerHTML = '&#10094;';
 next.innerHTML = '&#10095;';
 prev.classList.add('prev');
 next.classList.add('next');
-myCarousel.appendChild(prev);
-myCarousel.appendChild(next);
+prev.addEventListener('click', function(e){ showSlides(slideIndex -= 1) });
+next.addEventListener('click', function(e){ showSlides(slideIndex += 1) });
 
-//ACTIONS FOR THE ARROWS
-prev.addEventListener('click', function(e){ plusSlides(-1) });
-next.addEventListener('click', function(e){ plusSlides(1) });
 
-//FIRST TIME THE PAGE LOAD IT WILL SET UP TO SHOW THE FIRST SLIDER
-var slideIndex = 1;
-showSlides(slideIndex);
+//MAIN FUNCTIONS
+function createDots (slidesNum){
+  //create the dots
+  for(i=1; i <= slidesNum; i++){
+    let dot = document.createElement('button');
+    dot.setAttribute('type', 'button');
+    dot.setAttribute('data-key', i);
+    dot.classList.add('dot');
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+    dots.push(dot);
+    myCarousel.querySelector('.dot-container').appendChild(dot);
+  }
+
+  //the first dot active
+  dots[0].classList.add('active');
+
+  //create a on click function to change the slide
+  dots.map(function(dot){
+    dot.addEventListener('click', (e) => {
+      let key = dot.getAttribute('data-key');
+      showSlides(slideIndex = key);
+    })
+  })
 }
 
 function showSlides(n) {
-  var scroller = myCarousel.querySelector('.scroller');
-  var slides = scroller.querySelectorAll('.mySlide');
-
+  //prevent trying to reach a slide that does not exist
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
+
+  //handle dots
+  myCarousel.querySelector('.dot-container').querySelector('.dot.active').classList.remove('active');
+  dots.map(function(dot){
+    if(dot.getAttribute('data-key') == slideIndex){
+      dot.classList.add('active')
+    }
+  })
+
+  //handle the actual carousel
   scroller.style.transform = `translateX(-${(slideIndex - 1) * 100}%)`;
 }
+
+
+//FIRST TIME THE PAGE LOAD IT WILL SET UP TO SHOW THE FIRST SLIDER
+myCarousel.appendChild(prev);
+myCarousel.appendChild(next);
+createDots(slides.length)
+showSlides(slideIndex);
